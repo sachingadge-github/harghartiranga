53f591de0fc2bc4bef6bf4859c0f34fbc0a78966
sachingadge-github <59311240+sachingadge-github@users.noreply.github.com>
2022-07-22 00:22:21 +0530

    test chanage

--- index.php
+++ index.php
@@ -467,49 +467,7 @@
       class="container-fluid bg-dark footer mt-5 pt-5 wow fadeIn"
       data-wow-delay="0.1s"
     >
-      <div class="container py-5">
-        <div class="row g-5">
-          <div class="col-lg-4 col-md-6">
-            <img src="Azadi_logo.png" style="width: 70px;"  width="10%" alt=""> 
-            <img src="logo.png" style="width: 70px;"  width="10%" alt=""> 
-            
-            <h1 class="text-white mb-4">
-               हर घर तिरंगा
-            </h1>
-           
-            <div class="d-flex pt-2">
-              <a class="btn btn-square btn-outline-primary me-1" href=""
-                ><i class="fab fa-twitter"></i
-              ></a>
-              <a class="btn btn-square btn-outline-primary me-1" href=""
-                ><i class="fab fa-facebook-f"></i
-              ></a>
-              <a class="btn btn-square btn-outline-primary me-1" href=""
-                ><i class="fab fa-youtube"></i
-              ></a>
-              <a class="btn btn-square btn-outline-primary me-0" href=""
-                ><i class="fab fa-linkedin-in"></i
-              ></a>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6">
-            <h4 class="text-light mb-4">Address</h4>
-            <p>
-              <i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA
-            </p>
-            <p><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-            <p><i class="fa fa-envelope me-3"></i>info@example.com</p>
-          </div>
-          <div class="col-lg-4 col-md-6">
-            <h4 class="text-light mb-4">Quick Links</h4>
-            <a class="btn btn-link" href="">About Us</a>
-            <a class="btn btn-link" href="">Contact Us</a>
-            <a class="btn btn-link" href="">Our Services</a>
-            <a class="btn btn-link" href="">Terms & Condition</a>
-            <a class="btn btn-link" href="">Support</a>
-          </div>
-        </div>
-      </div>
+     
       <div class="container-fluid copyright">
         <div class="container">
           <div class="row">
